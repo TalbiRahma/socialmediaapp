85a3cbf5e3876f0d382140824fcb03f445c654a7
talbi rahma <talbirahma73@gmail.com>
2024-01-22 17:32:25 +0100

    first Register commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.get('/', (req,res)=>{
+
+    res.send('hello social media app')
+})
+
+const userRoutes = require('./routes/user.router')
+app.use('/users', userRoutes)
+mongoose.connect(process.env.CONNECTION_STRING ,
+    {
+        useNewUrlParser : true,
+        useUnifiedTopology : true
+    }
+)
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error : "));
+db.once("open", function(){
+    console.log("database connected sucessfully ...")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`app listing on port ${process.env.PORT}`)
+})
